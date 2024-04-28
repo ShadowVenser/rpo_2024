0b1c4f98f2256d1b7b6739b5b53bc76012caaf02
ShadowVenser <aeaes1215@gmail.com>
2024-04-28 23:52:02 +0300

    Lab_01
    Signed-off-by: ShadowVenser <aeaes1215@gmail.com>

---
+++ fclient/app/src/main/cpp/libs/mbedtls/build/x86/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/vss_admin/Projects/rpo_2024/libs/mbedtls/mbedtls
+# Build directory: /home/vss_admin/Projects/rpo_2024/libs/mbedtls/build/x86
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("framework")
+subdirs("include")
+subdirs("3rdparty")
+subdirs("library")
+subdirs("pkgconfig")
+subdirs("programs")
+subdirs("tests")
---
+++ fclient/app/src/main/java/rpo2024/iu3/fclient/MainActivity.java
@@ -0,0 +1,52 @@
+package rpo2024.iu3.fclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import rpo2024.iu3.fclient.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'fclient' library on application startup.
+    static {
+        System.loadLibrary("fclient");
+        System.loadLibrary("mbedcrypto");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int rng = initRng();
+        byte[] data = randomBytes(10);
+        byte[] key = randomBytes(16);
+
+        byte[] code = encrypt(key, data);
+        byte[] dec_data = decrypt(key, code);
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'fclient' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    public static native int initRng();
+
+    public static native byte[] randomBytes(int no);
+
+    public static native byte[] encrypt(byte[] key, byte[] data);
+
+    public static native byte[] decrypt(byte[] key, byte[] data);
+}
