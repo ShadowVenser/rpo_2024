56fa5f704af1eb6d62426761bce16ddb401f6fd8
ShadowVenser <aeaes1215@gmail.com>
2024-04-29 04:03:28 +0300

    Lab_02
    Signed-off-by: ShadowVenser <aeaes1215@gmail.com>

--- fclient/app/src/main/java/rpo2024/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/rpo2024/iu3/fclient/MainActivity.java
@@ -1,11 +1,25 @@
 package rpo2024.iu3.fclient;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.DeadObjectException;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import rpo2024.iu3.fclient.databinding.ActivityMainBinding;
+import org.apache.commons.codec.Decoder;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private ActivityMainBinding binding;
+    private Button btnClick;
+    private ActivityResultLauncher activityResultLauncher;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +49,43 @@ public class MainActivity extends AppCompatActivity {
         byte[] dec_data = decrypt(key, code);
 
         // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+        btnClick = (Button) findViewById(R.id.btnClick);
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(v.getContext(), PinpadActivity.class);
+                activityResultLauncher.launch(it);
+
+/*              byte[] key = strToHex("0123456789ABCDEF0123456789ABCDE0");
+                byte[] enc = encrypt(key, strToHex("1234"));
+                byte[] dec = decrypt(key, enc);
+                String ans = new String(Hex.encodeHex(dec)).toUpperCase();*/
+            }
+        });
+
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult res) {
+                        if (res.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = res.getData();
+                            String pin = data.getStringExtra("pin");
+                            Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public static byte[] strToHex(String str){
+        byte[] hex;
+        try {
+            hex = Hex.decodeHex(str.toCharArray());
+        }
+        catch (DecoderException e) {
+            hex = null;
+        }
+        return hex;
     }
 
     /**
